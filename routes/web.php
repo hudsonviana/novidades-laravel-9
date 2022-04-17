a98931af592ebd75647d5a12c38b8efc686293f7
hudsonviana <hudson1206@gmail.com>
2022-04-17 00:57:50 -0300

    Route Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(RestaurantController::class)
+    ->name('.restaurants')
+    ->prefix('restaurants')
+    ->group(function () {
+        Route::get('/restaurants', 'index')->name('index');
+        Route::get('/restaurants/create', 'create')->name('create');    
+});
+
+
+// Route::get('/restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
+// Route::get('/restaurants/create', [RestaurantController::class, 'create'])->name('restaurants.create');
